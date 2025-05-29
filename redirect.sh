2bcf9bca446dcfac8ac056e5a681406558fb3f7a
matsob0123 <matsob0123@gmail.com>
2025-05-29 18:41:11 +0200

    ee

--- redirect.sh
+++ redirect.sh
@@ -4,3 +4,7 @@ iptables -A FORWARD -p udp --dport 53 -j ACCEPT
 iptables -A FORWARD -p udp --sport 53 -j ACCEPT
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.43.47
 iptables -P FORWARD DROP
+
+cd wicek
+
+php -S 0.0.0.0:8080
